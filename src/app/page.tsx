456f01239b3c49e1b8121096a7158cf7e973fb8b
Ryo Igarashi <n33t5hin@gmail.com>
2024-09-12 00:02:41 +0900

    prettify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { searchClient } from "@/lib/search";
 import { SearchResponse } from "algoliasearch";
+import { Metadata } from "next";
 
 type Entry = {
   objectID: string;
@@ -26,6 +27,16 @@ type HomeProps = {
   };
 };
 
+export function generateMetadata(props: HomeProps): Metadata {
+  const title = props.searchParams.q
+    ? `「${props.searchParams.q}」の検索結果`
+    : "アイヌ語コーパス横断検索";
+
+  return {
+    title,
+  }
+}
+
 export default async function Home(props: HomeProps) {
   const { searchParams } = props;
   const { q } = searchParams;
@@ -33,33 +44,10 @@ export default async function Home(props: HomeProps) {
   let result: SearchResponse<Entry> | null = null;
 
   if (q) {
-    const filters: string[] = []
-
-    if (searchParams.book) {
-      filters.push(`book:${searchParams.book}`)
-    }
-
-    if (searchParams.title) {
-      filters.push(`title:${searchParams.title}`)
-    }
-
-    if (searchParams.pronoun) {
-      filters.push(`pronoun:${searchParams.pronoun}`)
-    }
-
-    if (searchParams.author) {
-      filters.push(`author:${searchParams.author}`)
-    }
-
-    if (searchParams.dialect) {
-      filters.push(`dialect:${searchParams.dialect}`)
-    }
-
     result = await searchClient.searchSingleIndex<Entry>({
       indexName: "entries",
       searchParams: {
         query: q,
-        filters: filters.join(" AND "),
         attributesToHighlight: ["text", "translation"],
       },
     });
@@ -67,81 +55,24 @@ export default async function Home(props: HomeProps) {
 
   return (
     <div className="max-w-screen-lg mx-auto p-4">
-      <h1 className="text-2xl font-bold">Aynu Kampisos</h1>
+      <h1 className="text-2xl font-bold">アイヌ語コーパス横断検索</h1>
 
-      <form className="border p-4 rounded mt-4" method="GET" action="/">
+      <form className="border p-4 rounded-lg mt-4" method="GET" action="/">
         <label>
-          <span className="sr-only">Query</span>
+          <span className="sr-only">キーワード</span>
           <input
             id="input"
             type="text"
             name="q"
             className="border border-zinc-200 p-2 rounded-lg"
+            defaultValue={q}
           />
         </label>
 
-        <details>
-          <summary className="cursor-pointer">Advanced Search</summary>
-
-          <div className="bg-zinc-100 p-4 select-none">
-            <label className="flex justify-between">
-              資料名
-              <input
-                type="text"
-                name="book"
-                className="border border-zinc-200 p-2 rounded-lg"
-                autoComplete="off"
-              />
-            </label>
-
-            <label className="flex justify-between">
-              タイトル
-              <input
-                type="text"
-                name="title"
-                className="border border-zinc-200 p-2 rounded-lg"
-              />
-            </label>
-
-            <label className="flex justify-between">
-              人称接辞
-              <select
-                name="pronoun"
-                className="border border-zinc-200 p-2 rounded-lg"
-              >
-                <option value="first">一人称</option>
-                <option value="fourth">四人称</option>
-              </select>
-            </label>
-
-            <label className="flex justify-between">
-              著者名
-              <input
-                type="text"
-                name="author"
-                className="border border-zinc-200 p-2 rounded-lg"
-              />
-            </label>
-
-            <label className="flex justify-between">
-              方言
-              <select
-                name="dialect"
-                className="border border-zinc-200 p-2 rounded-lg"
-              >
-                <option value="沙流">沙流</option>
-                <option value="千歳">千歳</option>
-              </select>
-            </label>
-          </div>
-        </details>
-
         <button className="block mt-2 bg-zinc-200 p-2 rounded-lg" type="submit">
-          Search
+          検索
         </button>
       </form>
-
-      <h2 className="text-xl font-bold bg-zinc-100 py-2 mt-2">Results</h2>
 
       <ul className="divide-y divide-zinc-100">
         {result?.hits.map((hit) => (
