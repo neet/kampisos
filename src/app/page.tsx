ddfb2793cdc50ca81b148980c19de8e4e480c2ee
Ryo Igarashi <n33t5hin@gmail.com>
2024-09-12 14:11:26 +0900

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { searchClient } from "@/lib/search";
 import { SearchResponse } from "algoliasearch";
 import { Metadata } from "next";
+import Link from "next/link";
 
 type Entry = {
   objectID: string;
@@ -19,18 +20,14 @@ export const revalidate = 86_400;
 type HomeProps = {
   searchParams: {
     q?: string;
-    book?: string;
-    title?: string;
-    pronoun?: string;
-    author?: string;
-    dialect?: string;
+    page?: number;
   };
 };
 
 export function generateMetadata(props: HomeProps): Metadata {
   const title = props.searchParams.q
-    ? `「${props.searchParams.q}」の検索結果`
-    : "アイヌ語コーパス横断検索";
+    ? `「${props.searchParams.q}」の検索結果 | kampisos`
+    : "kampisos - アイヌ語コーパス横断検索";
 
   return {
     title,
@@ -40,6 +37,7 @@ export function generateMetadata(props: HomeProps): Metadata {
 export default async function Home(props: HomeProps) {
   const { searchParams } = props;
   const { q } = searchParams;
+  const page = Number(searchParams.page ?? 0);
 
   let result: SearchResponse<Entry> | null = null;
 
@@ -48,6 +46,7 @@ export default async function Home(props: HomeProps) {
       indexName: "entries",
       searchParams: {
         query: q,
+        page,
         attributesToHighlight: ["text", "translation"],
       },
     });
@@ -55,24 +54,39 @@ export default async function Home(props: HomeProps) {
 
   return (
     <div className="max-w-screen-lg mx-auto p-4">
-      <h1 className="text-2xl font-bold">アイヌ語コーパス横断検索</h1>
+      <h1 className="text-2xl font-bold">kampisos</h1>
 
-      <form className="border p-4 rounded-lg mt-4" method="GET" action="/">
-        <label>
-          <span className="sr-only">キーワード</span>
+      <form method="GET" action="/">
+        <search className="flex max-w-screen-sm mx-auto">
           <input
             id="input"
             type="text"
             name="q"
-            className="border border-zinc-200 p-2 rounded-lg"
+            className="block flex-1 box-border border border-r-0 border-zinc-300 py-2 px-5 text-lg rounded-lg rounded-r-none"
             defaultValue={q}
+            autoCapitalize="off"
+            autoCorrect="off"
+            autoComplete="off"
+            spellCheck="false"
           />
-        </label>
 
-        <button className="block mt-2 bg-zinc-200 p-2 rounded-lg" type="submit">
-          検索
-        </button>
+          <button className="block text-lg px-5 py-2 rounded-lg rounded-l-none bg-blue-600 text-white" type="submit">
+            検索
+          </button>
+        </search>
       </form>
+
+      {result && (
+        <div className="flex gap-2 text-zinc-500 justify-end text-sm">
+          <span>
+            {result.nbHits}件の検索結果
+          </span>
+
+          <span>
+            {result.processingTimeMS}ミリ秒
+          </span>
+        </div>
+      )}
 
       <ul className="divide-y divide-zinc-100">
         {result?.hits.map((hit) => (
@@ -94,8 +108,8 @@ export default async function Home(props: HomeProps) {
             </div>
 
             <div className="flex gap-4 flex-wrap mt-2 text-zinc-500 text-sm justify-end">
-              <div>{hit.book}</div>
-              <div>{hit.title}</div>
+              {/* <div>{hit.book}</div>
+              <div>{hit.title}</div> */}
               <div>{hit.pronoun}</div>
               <div>{hit.author}</div>
               <div>{hit.dialect}</div>
@@ -103,6 +117,24 @@ export default async function Home(props: HomeProps) {
           </li>
         ))}
       </ul>
+
+      <div>
+        <ul className="flex justify-center gap-2">
+          {
+            Array.from({ length: result?.nbPages ?? 0 }).map((_, i) => (
+              <li key={i}>
+                {
+                  i === page ? (
+                    <span>{i + 1}</span>
+                  ): (
+                    <Link href={`/?q=${q}&page=${i}`} className="underline text-blue-600">{i + 1}</Link>
+                  )
+                }
+              </li>
+            ))
+          }
+        </ul>
+      </div>
     </div>
   );
 }
