113aba5dab5efcdb15708785cffdd0686a805069
Ryo Igarashi <n33t5hin@gmail.com>
2024-09-15 14:59:49 +0900

    Use transition for navigation

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -2,7 +2,7 @@
 
 import clsx from "clsx";
 import { useRouter } from "next/navigation";
-import { FC } from "react";
+import { FC, useTransition } from "react";
 
 export type SearchProps = {
   className?: string;
@@ -13,21 +13,24 @@ export const Search: FC<SearchProps> = (props) => {
   const { className, defaultValue } = props;
 
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+    startTransition(() => {
+      event.preventDefault();
 
-    const form = event.currentTarget;
-    const formData = new FormData(form);
-    const q = formData.get("q") as string;
+      const form = event.currentTarget;
+      const formData = new FormData(form);
+      const q = formData.get("q") as string;
 
-    const searchParams = new URLSearchParams();
-    searchParams.set("q", q);
+      const searchParams = new URLSearchParams();
+      searchParams.set("q", q);
 
-    const url = new URL(form.action);
-    url.search = searchParams.toString();
+      const url = new URL(form.action);
+      url.search = searchParams.toString();
 
-    router.push(url.toString());
+      router.push(url.toString());
+    });
   };
 
   return (
@@ -68,10 +71,12 @@ export const Search: FC<SearchProps> = (props) => {
             "px-4 py-2",
             "rounded-lg rounded-l-none",
             "bg-zinc-950 text-white",
+            "disabled:bg-zinc-600",
           )}
           type="submit"
+          disabled={isPending}
         >
-          検索
+          {isPending ? "検索中..." : "検索"}
         </button>
       </div>
     </form>
