ad2e2b195e6a5e2f02b1ce7cb8a4ff8c8e21fbb1
Ryo Igarashi <n33t5hin@gmail.com>
2024-09-19 19:11:53 +0900

    Add URL to edit an entry

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,25 @@
 import clsx from "clsx";
-import { ComponentPropsWithoutRef, FC, ReactNode } from "react";
+import { ComponentPropsWithoutRef, ElementType, FC, ReactNode } from "react";
+import { overridable } from "react-as-prop";
 
-export type ButtonProps = {
+type ButtonProps = {
+  as: ElementType;
   variant?: "primary" | "secondary";
   children: ReactNode;
 } & ComponentPropsWithoutRef<"button">;
 
-export const Button: FC<ButtonProps> = (props) => {
-  const { variant = "primary", children, ...rest } = props;
+const _Button: FC<ButtonProps> = (props) => {
+  const {
+    as: Component,
+    variant = "primary",
+    children,
+    className,
+    ...rest
+  } = props;
 
-  if (variant === "primary") {
-    return (
-      <button
-        className={clsx(
+  const classNames =
+    variant === "primary"
+      ? clsx(
           "px-3 py-2",
           "rounded-lg",
           "bg-black text-white",
@@ -20,28 +27,22 @@ export const Button: FC<ButtonProps> = (props) => {
           "dark:bg-white dark:text-black",
           "dark:disabled:bg-zinc-200",
           "forced-colors:border forced-colors:border-[ButtonBorder]",
-        )}
-        {...rest}
-      >
-        {children}
-      </button>
-    );
-  }
-
-  if (variant === "secondary") {
-    return (
-      <button
-        className={clsx(
+          className,
+        )
+      : clsx(
           "px-3 py-2",
           "rounded-lg",
           "border border-black text-black",
           "dark:border-white dark:text-white",
           "forced-colors:border forced-colors:border-[ButtonBorder]",
-        )}
-        {...rest}
-      >
-        {children}
-      </button>
-    );
-  }
+          className,
+        );
+
+  return (
+    <Component className={classNames} {...rest}>
+      {children}
+    </Component>
+  );
 };
+
+export const Button = overridable(_Button, "button");
--- src/components/Entry/EntryDetailsViewer.tsx
+++ src/components/Entry/EntryDetailsViewer.tsx
@@ -18,6 +18,22 @@ export const EntryDetailsViewer: FC<EntryDetailsViewerProps> = (props) => {
   const { text, translation, book, title, url, author, dialect, onClose } =
     props;
 
+  const feedbackURL = new URL(
+    "/forms/d/e/1FAIpQLSdF8BWlXRjy1uAnKQ9-740_P016PD61gMZxQ4cQVlt7Aj9r7Q/viewform",
+    "https://docs.google.com",
+  );
+
+  feedbackURL.search = new URLSearchParams([
+    ["usp", "pp_url"],
+    ["entry.1073831692", text],
+    ["entry.283613557", translation],
+    ["entry.28800350", book],
+    ["entry.1935383913", title],
+    ["entry.1034703594", url],
+    ["entry.240850099", author ?? ""],
+    ["entry.1920517713", dialect ?? ""],
+  ]).toString();
+
   return (
     <div>
       <dl className="mt-4 grid grid-cols-[65px,auto] gap-x-4 gap-y-2">
@@ -54,6 +70,15 @@ export const EntryDetailsViewer: FC<EntryDetailsViewerProps> = (props) => {
       </dl>
 
       <div className="mt-4 flex justify-end gap-2 items-center">
+        <Button
+          as="a"
+          href={feedbackURL.toString()}
+          target="_blank"
+          rel="noreferrer"
+          variant="secondary"
+        >
+          修正を依頼
+        </Button>
         <Button onClick={onClose}>閉じる</Button>
       </div>
     </div>
