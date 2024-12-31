59f2a1e65f203b6c4a925a7afef24075190d6970
Ryo Igarashi <n33t5hin@gmail.com>
2024-12-31 14:25:49 +0900

    chore: Restore sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,17 @@
+import fs from "fs/promises";
 import type { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const ainuWordFile = await fs.readFile("public/words_ain.tsv", "utf-8");
+  const ainuWordList = ainuWordFile
+    .split("\n")
+    .map((line) => line.split("\t")[0]);
+
+  const japaneseWordFile = await fs.readFile("public/words_jpn.tsv", "utf-8");
+  const japaneseWordList = japaneseWordFile
+    .split("\n")
+    .map((line) => line.split("\t")[0]);
+
   return [
     {
       url: "https://kampisos.aynu.io",
@@ -12,5 +23,21 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       lastModified: new Date(),
       changeFrequency: "monthly",
     },
+
+    ...ainuWordList.map((word) => {
+      return {
+        url: `https://kampisos.aynu.io/search?q=${encodeURIComponent(word)}`,
+        lastModified: new Date(),
+        changeFrequency: "monthly" as const,
+      };
+    }),
+
+    ...japaneseWordList.map((word) => {
+      return {
+        url: `https://kampisos.aynu.io/search?q=${encodeURIComponent(word)}`,
+        lastModified: new Date(),
+        changeFrequency: "monthly" as const,
+      };
+    }),
   ];
 }
