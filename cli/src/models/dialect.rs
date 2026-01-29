40101658affc425a15aa6a784ad10b19bc9d6fba
Ryō Igarashi (五十嵐 涼) <n33t5hin@gmail.com>
2026-01-29 22:28:39 +0900

    chore: Remove unused typesense (#22)

--- cli/src/models/dialect.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(untagged)]
-pub enum Dialect {
-    Path(String),
-    NamedPath { name: String, path: String },
-    NamedPathList { name: String, paths: Vec<String> },
-}
--- cli/src/models/mod.rs
+++
@@ -1,7 +0,0 @@
-mod article;
-mod dialect;
-mod sentence;
-
-pub use article::*;
-pub use dialect::*;
-pub use sentence::*;
